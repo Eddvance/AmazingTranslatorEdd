fe302445bcc14b8efe979a77a47e546927bd5c2d
Edouard Leroy <leroyedouard@protonmail.com>
2025-01-15 17:00:03 +0100

    suppression des instantiations repetees ; remplacement du scanner par inputReader(en prevision des futurs tests) ; modification divers dans Execut

--- src/main/java/io/eddvance/practice/execution/Execut.java
+++ src/main/java/io/eddvance/practice/execution/Execut.java
@@ -3,43 +3,44 @@ package io.eddvance.practice.execution;
 import io.eddvance.practice.App;
 import io.eddvance.practice.choices.choice_number.NumberToChoice;
 import io.eddvance.practice.choices.choices_translation.TranslationToChoice;
+import io.eddvance.practice.interaction.InputReader;
 import io.eddvance.practice.translation.data.FrenchTranslation;
 import io.eddvance.practice.translation.data.GermanTranslation;
 import io.eddvance.practice.translation.translator.SearchAndReturn;
 
-import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class Execut {
+    private final Logger logger = Logger.getLogger(App.class.getName());
+    private InputReader inputReader;
+    private final FrenchTranslation frenchTranslation = new FrenchTranslation();
+    private final GermanTranslation germanTranslation = new GermanTranslation();
+    private final SearchAndReturn searchAndReturn = new SearchAndReturn();
+    private final TranslationToChoice translationToChoice = new TranslationToChoice();
+    private final NumberToChoice numberToChoice = new NumberToChoice();
+    public Execut() {
+        this.inputReader = new InputReader();
+    }
 
     public void action() {
-        final Logger logger = Logger.getLogger(App.class.getName());
-        final Scanner scanner = new Scanner(System.in);
         String restart;
 
         do {
             String translation = getString();
             logger.fine(() -> STR."Your translation is : \{translation}");
             logger.info("Do you want to translate another number? (y)");
-            restart = scanner.nextLine();
+            restart = inputReader.readLine();
         }
         while (restart.equalsIgnoreCase("y"));
         logger.warning("Au revoir - Auf Wiedersehen");
     }
 
-    private static String getString() {
-        FrenchTranslation frenchTranslation = new FrenchTranslation();
+    private String getString() {
         frenchTranslation.frenchTranslation();
-        GermanTranslation germanTranslation = new GermanTranslation();
         germanTranslation.germanTranslation();
-
-        SearchAndReturn searchAndReturn = new SearchAndReturn();
-        TranslationToChoice translationToChoice = new TranslationToChoice();
-        NumberToChoice numberToChoice = new NumberToChoice();
 
         int number = numberToChoice.numberChoice();
         int languageSelection = translationToChoice.translationToChoice();
-
         return searchAndReturn.getTranslation(number, languageSelection);
     }
 }
